04bcc1621be215eca460c739fa197e2b30ddd60f
Hector Flores <hflores@nearsoft.comm>
2019-05-20 10:17:25 -0700

    Creating gh-pages

---
+++ build/precache-manifest.b2ef93831737bf75ce5ed6826351b70b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "139fa7dea6689a6f3ca0",
+    "url": "/static/js/main.139fa7de.chunk.js"
+  },
+  {
+    "revision": "e0739171895a9e42d4e9",
+    "url": "/static/js/1.e0739171.chunk.js"
+  },
+  {
+    "revision": "139fa7dea6689a6f3ca0",
+    "url": "/static/css/main.e563544e.chunk.css"
+  },
+  {
+    "revision": "e0739171895a9e42d4e9",
+    "url": "/static/css/1.ef326b09.chunk.css"
+  },
+  {
+    "revision": "e6318e8de12078a91ef1f23b51135512",
+    "url": "/index.html"
+  }
+];
